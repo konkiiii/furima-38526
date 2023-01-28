34a6471ff320b3572851980db527ae59de025b6a
konkiiii <kintaro0827@icloud.com>
2023-01-28 18:38:57 +0900

    RSpec設定
    rails g rspec:install
    rails g rspec:model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence:true do
+    validates :nickname
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+    validates :birth_day
+  end
 end
