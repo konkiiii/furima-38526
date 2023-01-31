f76113c7faee6978d83459ea8ffb42b467641612
konkiiii <kintaro0827@icloud.com>
2023-01-31 22:08:14 +0900

    Productモデルおよびテーブルの作成
    rails g model product
    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_26_130847) do
+ActiveRecord::Schema.define(version: 2023_01_31_125800) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "product_name", null: false
+    t.text "description", null: false
+    t.integer "category_genre_id", null: false
+    t.integer "condition_genre_id", null: false
+    t.integer "delivery_charge_genre_id", null: false
+    t.integer "prefectures_genre_id", null: false
+    t.integer "days_to_ship_genre_id", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2023_01_26_130847) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
