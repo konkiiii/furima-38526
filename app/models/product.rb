5db685a8148f172a31c80078b10892142f95a182
konkiiii <kintaro0827@icloud.com>
2023-01-31 23:02:29 +0900

    active_hashの導入
    bundle install
    app/models/product.rb
    のバリデーションの記載に注意(適用されないなら1つずつ記述)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,18 @@
 class Product < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_genre_id
+  belongs_to :condition_genre_id
+  belongs_to :delivery_charge_genre_id
+  belongs_to :prefectures_genre_id
+  belongs_to :days_to_ship_genre_id
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_genre_id
+    validates :condition_genre_id
+    validates :delivery_charge_genre_id
+    validates :prefectures_genre_id
+    validates :days_to_ship_genre_id
+  end
+  # この書き方でダメな場合は修正(1つずつ記述に変更)
 end
