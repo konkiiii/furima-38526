c4a0187bafcd271d65f903e5cffa1f1673ce8b5c
konkiiii <kintaro0827@icloud.com>
2023-02-04 11:00:37 +0900

    createアクション記述
    アクティブハッシュ関連でNameエラー出るから修正必要

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,17 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      render 'index'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :product_name, :description, :category_genre_id, :condition_genre_id, :delivery_charge_genre_id, :prefectures_genre_id, :days_to_ship_genre_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category_genre_id
   belongs_to :condition_genre_id
@@ -7,20 +8,25 @@ class Item < ApplicationRecord
   belongs_to :prefectures_genre_id
   belongs_to :days_to_ship_genre_id
 
-  with_options numericality: { other_than: 1, message: "can't be blank" } do
-    validates :category_genre_id
-    validates :condition_genre_id
-    validates :delivery_charge_genre_id
-    validates :prefectures_genre_id
-    validates :days_to_ship_genre_id
-  end
-  # この書き方でダメな場合は修正(1つずつ記述に変更)
+  # with_options numericality: { other_than: 1, message: "can't be blank" } do
+  #   validates :category_genre_id
+  #   validates :condition_genre_id
+  #   validates :delivery_charge_genre_id
+  #   validates :prefectures_genre_id
+  #   validates :days_to_ship_genre_id
+  # end
+  validates :category_genre_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :condition_genre_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_charge_genre_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefectures_genre_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :days_to_ship_genre_id, numericality: { other_than: 1, message: "can't be blank" } 
 
   belongs_to :user
 
   with_options presence:true do
     validates :user
     validates :product_name
-    validates :descriotion
+    validates :description
     validates :price
+  end
 end
