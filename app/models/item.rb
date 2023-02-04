b89475d74905b8b761c450a2c7c5f30880952b31
konkiiii <kintaro0827@icloud.com>
2023-02-04 14:44:36 +0900

    active_hashエラー解決
    モデル名変更
    xxx_genre_id.rb→xxx_genre.rb
    アソシエーション変更
    belongs_to :xxx_genre_id
    →belongs_to :xxx_genre

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,25 +2,20 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category_genre_id
-  belongs_to :condition_genre_id
-  belongs_to :delivery_charge_genre_id
-  belongs_to :prefectures_genre_id
-  belongs_to :days_to_ship_genre_id
+  belongs_to :category_genre
+  belongs_to :condition_genre
+  belongs_to :delivery_charge_genre
+  belongs_to :prefectures_genre
+  belongs_to :days_to_ship_genre
 
-  # with_options numericality: { other_than: 1, message: "can't be blank" } do
-  #   validates :category_genre_id
-  #   validates :condition_genre_id
-  #   validates :delivery_charge_genre_id
-  #   validates :prefectures_genre_id
-  #   validates :days_to_ship_genre_id
-  # end
-  validates :category_genre_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :condition_genre_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :delivery_charge_genre_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :prefectures_genre_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :days_to_ship_genre_id, numericality: { other_than: 1, message: "can't be blank" } 
-
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_genre_id
+    validates :condition_genre_id
+    validates :delivery_charge_genre_id
+    validates :prefectures_genre_id
+    validates :days_to_ship_genre_id
+  end
+  
   belongs_to :user
 
   with_options presence:true do
