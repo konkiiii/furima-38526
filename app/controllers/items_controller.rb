f7cff202b1e943216696a0158379e2a432c20169
konkiiii <kintaro0827@icloud.com>
2023-02-06 21:16:58 +0900

    修正_2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      render 'index'
+      redirect_to root_path
     else
       render 'new'
     end
