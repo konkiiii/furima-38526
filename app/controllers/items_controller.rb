ce7cb8f96289c63df272db2b5bb7b93be7788612
konkiiii <kintaro0827@icloud.com>
2023-02-13 19:22:43 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item,except: [:index, :new, :create]
+  before_action :set_item, except: [:index, :new, :create]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -48,6 +48,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :product_name, :description, :category_genre_id, :condition_genre_id,
                                  :delivery_charge_genre_id, :prefectures_genre_id, :days_to_ship_genre_id, :price).merge(user_id: current_user.id)
   end
+
   def set_item
     @item = Item.find(params[:id])
   end
